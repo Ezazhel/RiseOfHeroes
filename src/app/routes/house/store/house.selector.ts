f6171c5fb3b86e6b7cbde7377d7fd1ed54ed53a3
Ezazhel <Ezazhel@gmail.com>
2020-04-28 10:17:24 +0200

    new deployement

--- src/app/routes/house/store/house.selector.ts
+++ src/app/routes/house/store/house.selector.ts
@@ -1,9 +1,7 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { HouseState } from "./house.reducer";
 
-export const houseStateSelector = createFeatureSelector<HouseState>(
-    "houseState"
-);
+export const houseStateSelector = createFeatureSelector<HouseState>("house");
 
 export const trainingEquipement = createSelector(
     houseStateSelector,
